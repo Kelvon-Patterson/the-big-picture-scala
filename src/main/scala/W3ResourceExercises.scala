56819246ff2fa61fdd14cbd5443a233570d6917d
Kelvon-Patterson <kelvonpatterson044@gmail.com>
2022-08-22 10:41:35 -0500

    feat: Scala exercise 1

---
+++ src/main/scala/W3ResourceExercises.scala
@@ -0,0 +1,10 @@
+object W3ResourceExercises extends App {
+
+
+  // writye a Scala program to print "Hello World" and version of the scala language.
+
+  //1)
+
+  println("Hello World")
+  println("The version of the scala language being used is " + util.Properties.versionString)
+}
