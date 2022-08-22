3210b74c7af3c1a69c23a9d7b5241baae8c5feeb
Kelvon-Patterson <kelvonpatterson044@gmail.com>
2022-08-22 10:47:22 -0500

    feat: sum function

--- src/main/scala/W3ResourceExercises.scala
+++ src/main/scala/W3ResourceExercises.scala
@@ -1,10 +1,22 @@
 object W3ResourceExercises extends App {
 
 
-  // writye a Scala program to print "Hello World" and version of the scala language.
+  //1) write a Scala program to print "Hello World" and version of the scala language.
 
-  //1)
 
   println("Hello World")
   println("The version of the scala language being used is " + util.Properties.versionString)
+
+  //2) Write a Scala program to compute the sum of the two given integer values. If the two values are the same then triple the sum.
+
+  def sum( a: Int, b: Int): Int = {
+    if(a == b){
+      (a+b) * 3
+    }
+    else
+    a+b
+  }
+
+  println(sum(3,3))
+  println(sum(3,5))
 }
