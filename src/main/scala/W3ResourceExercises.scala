509ac72ba9044ec089c140eb380a9a7f11a53786
Kelvon-Patterson <kelvonpatterson044@gmail.com>
2022-08-22 11:02:07 -0500

    feat: absDifference Solution

--- src/main/scala/W3ResourceExercises.scala
+++ src/main/scala/W3ResourceExercises.scala
@@ -1,3 +1,5 @@
+import scala.math.abs
+
 object W3ResourceExercises extends App {
 
 
@@ -17,6 +19,15 @@ object W3ResourceExercises extends App {
     a+b
   }
 
-  println(sum(3,3))
-  println(sum(3,5))
+  println( sum(3,3))
+  println( sum(3,5))
+
+  //3) Write a Scala program to get the absolute difference between n and 51. If n is greater than 51 return triple the absolute difference.
+
+  def absoluteDiff(n:Int)= {
+    val absValDiff = Math.abs(n-51)
+    if(n>51) 3* absValDiff else absValDiff
+  }
+  println(absoluteDiff(40))
+  println(absoluteDiff(60))
 }
