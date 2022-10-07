ebd35793619a642186af46bee199628e2278ae54
Kelvon-Patterson <kelvonpatterson044@gmail.com>
2022-10-07 11:15:55 -0500

    feat: Arrays

---
+++ src/main/scala/DataFlairTutorial/Arrays.scala
@@ -0,0 +1,19 @@
+package DataFlairTutorial
+
+object Arrays extends App {
+
+  //TODO: What is Scala Array?
+  //When we want to hold a number of elements of the same kind in a collection, we use an array.
+  //We can declare an array in two ways:
+
+  var a = new Array[Int](3)
+ a =  Array(0,0,0)
+  a(1) = 2
+  println(a.mkString("Array(", ", ", ")"))
+
+  var b = Array(0,0,0)
+
+
+
+
+}
---
+++ src/main/scala/DataFlairTutorial/WhileLoops.scala
@@ -0,0 +1,9 @@
+package DataFlairTutorial
+
+object WhileLoops extends App {
+
+  //TODO :  A while loop that will execute a set of statements as long as a condition is true.
+
+
+
+}
